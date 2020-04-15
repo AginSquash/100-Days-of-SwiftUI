f833e35c5e1a4a801a65f6c94867b33ad34ff9e4
AginS <agins.main@gmail.com>
2020-04-15 20:20:03 +0300

    made custom Astronaut Preview

---
+++ Days 39 - 42 Project 8 (Moonshot)/Moonshot/Moonshot/AstronautPreview.swift
@@ -0,0 +1,48 @@
+//
+//  AstronautPreview.swift
+//  Moonshot
+//
+//  Created by Vlad Vrublevsky on 15.04.2020.
+//  Copyright © 2020 Vlad Vrublevsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct AstronautPreview: View {
+    let member: CrewMember
+    
+    var body: some View
+    {
+        VStack {
+            Image(member.astronaut.id)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.primary, lineWidth: 1))
+            
+            Text(member.astronaut.name)
+                .font(.headline)
+                .frame(width: 150, alignment: .center)
+            Text(member.role)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
+        }
+        .padding()
+        .frame(width: 160, height: 250, alignment: .top)
+    }
+}
+
+struct AstronautPreview_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    static let member1: CrewMember = CrewMember(role: "Pilot", astronaut: astronauts.first!)
+    static let member2: CrewMember = CrewMember(role: "Pilot", astronaut: astronauts[1])
+    
+    static var previews: some View {
+        Group {
+            AstronautPreview(member: member1)
+            AstronautPreview(member: member2)
+        }
+        .previewLayout(.fixed(width: 160, height: 250))
+    }
+}
---
+++ Days 39 - 42 Project 8 (Moonshot)/Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,76 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Vlad Vrublevsky on 14.04.2020.
+//  Copyright © 2020 Vlad Vrublevsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
+
+struct MissionView: View {
+    let mission: Mission
+    let astronauts: [CrewMember]
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: geometry.size.width * 0.7)
+                    .padding(.top)
+                    
+                    Text(self.mission.description)
+                    .padding()
+                    
+                    Spacer(minLength: 15)
+                    
+                    Text("Crew:")
+                        .font(.title)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(self.astronauts, id:\.role) { crewMember in
+                                    AstronautPreview(member: crewMember)
+                                }
+                        }
+                    }
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationBarTitle(Text(self.mission.displayName), displayMode: .inline)
+    }
+    
+    
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+        
+        var matches = [CrewMember]()
+        
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing \(member)")
+            }
+        }
+        self.astronauts = matches
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        MissionView(mission: missions[0], astronauts: astronauts)
+    }
+}
