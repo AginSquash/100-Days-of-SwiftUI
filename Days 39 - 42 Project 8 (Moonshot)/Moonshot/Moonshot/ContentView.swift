1b21813beddc978d7660fbc07c7e32c118eedd7e
AginS <agins.main@gmail.com>
2020-04-15 23:12:50 +0300

    wrap up is finish

--- Days 39 - 42 Project 8 (Moonshot)/Moonshot/Moonshot/ContentView.swift
+++ Days 39 - 42 Project 8 (Moonshot)/Moonshot/Moonshot/ContentView.swift
@@ -14,24 +14,46 @@ struct ContentView: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    @State private var sortByName = false
+    
     var body: some View {
         NavigationView {
-            List(missions) { mission in
-                NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts, Missions: self.missions)) {
-                    Image(mission.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 44, height: 44)
-                    
-                    VStack (alignment: .leading) {
-                        Text(mission.displayName)
-                            .font(.title)
-                        Text(mission.formattedLaunchDate)
+            VStack {
+                Toggle(isOn: $sortByName) {
+                    Text("Sort by name")
+                        .font(.headline)
+                }
+                .padding()
+                
+                List(missions) { mission in
+                    NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts, Missions: self.missions)) {
+                        Image(mission.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 44, height: 44)
+                        
+                        VStack (alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.title)
+                            Text( self.sortByName ? self.getAstronautsName(crews: mission.crew) : mission.formattedLaunchDate)
+                            
+                        }
                     }
                 }
             }
             .navigationBarTitle("Moonshot")
         }
+    }
+    
+    func getAstronautsName(crews: [Mission.CrewRole]) -> String
+    {
+        var memberString = String()
+        for crew in crews {
+            if let member = astronauts.first(where: { $0.id == crew.name } ) {
+                memberString.append( "\(member.name), " )
+            }
+        }
+        return String (memberString.dropLast(2))
     }
 }
 
