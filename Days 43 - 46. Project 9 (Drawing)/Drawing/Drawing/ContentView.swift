615c678070357b130b932cfdc8bc84978a5470ba
AginS <agins.main@gmail.com>
2020-04-20 01:03:56 +0300

    project 9 fin .

--- Days 43 - 46. Project 9 (Drawing)/Drawing/Drawing/ContentView.swift
+++ Days 43 - 46. Project 9 (Drawing)/Drawing/Drawing/ContentView.swift
@@ -9,47 +9,70 @@
 import SwiftUI
 
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
 
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: CGFloat(value))
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.5)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+    public var animatableData: AnimatablePair<Double,Double> {
+        get { AnimatablePair(Double(rows), Double(columns)) }
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
             }
         }
-    .drawingGroup()
-    }
 
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
-
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+        return path
     }
 }
 
 
+struct Arrow: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY / 4))
+        path.addLine(to: CGPoint(x: rect.maxX / 3, y: rect.maxY / 4))
+        path.addLine(to: CGPoint(x: rect.maxX / 3, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX / 1.5, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX / 1.5, y: rect.maxY / 4))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY / 4))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var rows = 4
+    @State private var columns = 4
 
     var body: some View {
-        VStack {
-            ColorCyclingCircle(amount: self.colorCycle)
-                .frame(width: 300, height: 300)
-
-            Slider(value: $colorCycle)
-        }
+        Arrow()
+            .frame(width: 300, height: 300, alignment: .center)
     }
 }
 
