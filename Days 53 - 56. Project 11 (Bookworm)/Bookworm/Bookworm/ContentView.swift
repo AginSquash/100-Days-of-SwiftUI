924c5f5e6699cada49643c05b741be392ef3dcbb
AginS <agins.main@gmail.com>
2020-04-25 01:03:56 +0300

    complete day 55

--- Days 53 - 56. Project 11 (Bookworm)/Bookworm/Bookworm/ContentView.swift
+++ Days 53 - 56. Project 11 (Bookworm)/Bookworm/Bookworm/ContentView.swift
@@ -8,38 +8,62 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-
-    var body: some View {
-        Button(title) {
-            self.isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
-    @State private var rememberMe = false
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors:[
+        NSSortDescriptor(keyPath: \Book.title, ascending: true),
+        NSSortDescriptor(keyPath: \Book.author, ascending: true)
+    ]) var books: FetchedResults<Book>
 
-    var body: some View {
-        VStack {
-            PushButton(title: "Remember Me", isOn: $rememberMe)
-            Text(rememberMe ? "On" : "Off")
+    @State private var showingAddScreen = false
+    
+   var body: some View {
+             NavigationView {
+                List {
+                    ForEach(books, id: \.self) { book in
+                        NavigationLink(destination: DetailView(book: book))
+                        {
+                            EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                .onDelete(perform: deleteBooks)
+                }
+                    
+                .navigationBarTitle("Bookworm")
+                .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                        self.showingAddScreen.toggle()
+                    }) {
+                        Image(systemName: "plus")
+                    })
+                    .sheet(isPresented: $showingAddScreen) {
+                        AddBookView().environment(\.managedObjectContext, self.moc)
+                    }
+            }
+    }
+    
+    func deleteBooks(at offsets: IndexSet)
+    {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
         }
+        try? moc.save()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        //Fix for preview
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        return ContentView().environment(\.managedObjectContext, context)
     }
 }
