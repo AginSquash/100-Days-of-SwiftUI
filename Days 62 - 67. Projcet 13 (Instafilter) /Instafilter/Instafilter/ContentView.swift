d77b3b7e33ad02eaf461386ba1d79bce2887b25a
AginS <agins.main@gmail.com>
2020-04-27 01:52:08 +0300

    day 63 is fin

---
+++ Days 62 - 67. Projcet 13 (Instafilter) /Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Vlad Vrublevsky on 26.04.2020.
+//  Copyright © 2020 Vlad Vrublevsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+
+            Button("Select Image") {
+               self.showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
