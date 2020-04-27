3f54a18b6fcd513ecad15914b199164b73af8299
AginS <agins.main@gmail.com>
2020-04-27 16:26:55 +0300

    day 65 (2/3)

--- Days 62 - 67. Projcet 13 (Instafilter) /Instafilter/Instafilter/ContentView.swift
+++ Days 62 - 67. Projcet 13 (Instafilter) /Instafilter/Instafilter/ContentView.swift
@@ -10,10 +10,61 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    @State private var showImagePicker = false
+    @State private var inputImage: UIImage?
+    
     var body: some View {
-        Text("123")
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+                    
+                    if  image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                            Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    self.showImagePicker = true
+                }
+            HStack {
+                Text("Intensity")
+                Slider(value: self.$filterIntensity)
+            }.padding(.vertical)
+
+            HStack {
+                Button("Change Filter") {
+                        // change filter
+                    }
+
+                    Spacer()
+
+                    Button("Save") {
+                            // save the picture
+                        }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
+        }
+        .sheet(isPresented: $showImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
+        }
     }
     
+    func loadImage()
+    {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
