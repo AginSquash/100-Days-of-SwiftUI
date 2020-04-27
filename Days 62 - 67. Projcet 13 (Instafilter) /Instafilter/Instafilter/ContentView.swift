76f896eb399febbdddb63a1361b65fb76de6feb4
AginS <agins.main@gmail.com>
2020-04-27 15:08:45 +0300

    overiew is fin

--- Days 62 - 67. Projcet 13 (Instafilter) /Instafilter/Instafilter/ContentView.swift
+++ Days 62 - 67. Projcet 13 (Instafilter) /Instafilter/Instafilter/ContentView.swift
@@ -8,30 +8,13 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State private var inputImage: UIImage?
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-
-            Button("Select Image") {
-               self.showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$inputImage)
-        }
+        Text("123")
     }
     
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
