476d5c37863a9c1d4852a0e5ba5d8e1618b84e86
AginS <agins.main@gmail.com>
2020-04-30 02:21:34 +0300

    day 73 is fin

--- Days 68 - 73. Ptoject 14 (BucketList)/BucketList/BucketList/ContentView.swift
+++ Days 68 - 73. Ptoject 14 (BucketList)/BucketList/BucketList/ContentView.swift
@@ -8,56 +8,85 @@
 
 import SwiftUI
 import MapKit
+import LocalAuthentication
+
+struct unlockedView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+
+    @Binding var locations: [MKPointAnnotation]
+    @Binding var selectedPlace: MKPointAnnotation?
+    @Binding var showingPlaceDetails: Bool
+    @Binding var showingEditScreen: Bool
+    
+    var body: some View
+    {
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate,selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: self.locations)
+                   .edgesIgnoringSafeArea(.all)
+               Circle()
+                   .fill(Color.blue)
+                   .opacity(0.3)
+                   .frame(width: 32, height: 32)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            let newLocation = MKPointAnnotation()
+                            newLocation.coordinate = self.centerCoordinate
+                            newLocation.title = "Example location"
+                            self.locations.append(newLocation)
+                            self.selectedPlace = newLocation
+                            self.showingEditScreen = true
+                        }) {
+                            Image(systemName: "plus")
+                                .padding()
+                                .background(Color.black.opacity(0.75))
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .clipShape(Circle())
+                                .padding(.trailing)
+                        }
+                    }
+            }
+        }
+    }
+}
 
 struct ContentView: View {
-    @State private var centerCoordinate = CLLocationCoordinate2D()
+
     @State private var locations = [MKPointAnnotation]()
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEditScreen = false
+    @State private var isUnlocked = false
     
     var body: some View {
-       ZStack {
-        MapView(centerCoordinate: $centerCoordinate,selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: self.locations)
-               .edgesIgnoringSafeArea(.all)
-           Circle()
-               .fill(Color.blue)
-               .opacity(0.3)
-               .frame(width: 32, height: 32)
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        let newLocation = MKPointAnnotation()
-                        newLocation.coordinate = self.centerCoordinate
-                        newLocation.title = "Example location"
-                        self.locations.append(newLocation)
-                        self.selectedPlace = newLocation
+        
+        ZStack {
+            if isUnlocked {
+                unlockedView(locations: $locations, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, showingEditScreen: $showingEditScreen)
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+        }
+             .onAppear(perform: loadData)
+             .alert(isPresented: $showingPlaceDetails) {
+                Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
                         self.showingEditScreen = true
-                    }) {
-                        Image(systemName: "plus")
-                    }
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
+                    } )
+                }
+            .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
+                if self.selectedPlace != nil {
+                    EditView(placemark: self.selectedPlace!)
                 }
             }
-        }
-         .onAppear(perform: loadData)
-         .alert(isPresented: $showingPlaceDetails) {
-            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
-                    self.showingEditScreen = true
-                } )
-            }
-        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
-            if self.selectedPlace != nil {
-                EditView(placemark: self.selectedPlace!)
-            }
-        }
     }
     
     func getDocumentsDirectory() -> URL {
@@ -73,6 +102,28 @@ struct ContentView: View {
     func saveData() {
         let codableLocation = MKPointAnnotationToCodableMKPoint(points: self.locations)
         Bundle.main.writeCustomData(data: codableLocation, file: "SavedPlaces")
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authenticate yourself to unlock your places."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {  success, authenticationError in
+                
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+            }
+        } else {
+            // no biometric
+        }
     }
     
 }
