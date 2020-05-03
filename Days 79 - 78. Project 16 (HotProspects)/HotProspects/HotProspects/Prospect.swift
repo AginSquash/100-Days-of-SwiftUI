313b38808a61fa9ba78d0bd21617a9b6936c3804
AginS <agins.main@gmail.com>
2020-05-03 14:40:44 +0300

    day 84

--- Days 79 - 78. Project 16 (HotProspects)/HotProspects/HotProspects/Prospect.swift
+++ Days 79 - 78. Project 16 (HotProspects)/HotProspects/HotProspects/Prospect.swift
@@ -12,13 +12,40 @@ class Prospect: Identifiable, Codable {
     let id = UUID()
     var name = "Anonymous"
     var emailAddress = ""
-    var isContacted = false
+    fileprivate(set) var isContacted = false
+    let date: Date
 }
 
 class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    static let saveKey = "SavedData"
+    
+    @Published fileprivate(set) var people: [Prospect]
+    
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
     
     init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
         self.people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(self.people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
     }
 }
--- Days 79 - 78. Project 16 (HotProspects)/HotProspects/HotProspects/ProspectsView.swift
+++ Days 79 - 78. Project 16 (HotProspects)/HotProspects/HotProspects/ProspectsView.swift
@@ -7,6 +7,8 @@
 //
 
 import SwiftUI
+import CodeScanner
+import UserNotifications
 
 struct ProspectsView: View {
     enum FilterType {
@@ -24,8 +26,8 @@ struct ProspectsView: View {
             return "Uncontacted people"
         }
     }
-    @EnvironmentObject var prospects: Prospects
     
+    @EnvironmentObject var prospects: Prospects
     var filteredProspects: [Prospect] {
         switch filter {
         case .none:
@@ -37,28 +39,101 @@ struct ProspectsView: View {
         }
     }
     
+    @State private var isShowingScanner = false
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(filteredProspects) { prospect in
                     VStack(alignment: .leading) {
-                        Text(prospect.name)
-                            .font(.headline)
+                        HStack {
+                            Text(prospect.name)
+                                .font(.headline)
+                            Image(systemName: prospect.isContacted ? "checkmark.circle" : "questionmark.circle" )
+                        }
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .contextMenu(menuItems: {
+                        Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
+                            self.prospects.toggle(prospect)
+                        }
+                        
+                        if !prospect.isContacted {
+                            Button("Remind Me") {
+                                self.addNotification(for: prospect)
+                            }
+                        }
+                        
+                    })
                 }
             }
             .navigationBarTitle(title)
             .navigationBarItems(trailing: Button(action: {
-                let prospect = Prospect()
-                prospect.name = "Paul Hudson"
-                prospect.emailAddress = "paul@hackingwithswift.com"
-                self.prospects.people.append(prospect)
+                self.isShowingScanner = true
             }, label: {
                 Image(systemName: "qrcode.viewfinder")
                 Text("Scan")
             }))
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: self.handleScanner)
+                    .edgesIgnoringSafeArea(.bottom)
+            }
+        }
+    }
+    
+    func handleScanner(result: Result<String, CodeScannerView.ScanError> ) {
+        self.isShowingScanner = false
+        
+        switch result {
+        case .success(let code):
+            let details = code.components(separatedBy: "\n")
+            
+            guard details.count == 2 else { return }
+            
+            let newPerson = Prospect()
+            newPerson.name = details[0]
+            newPerson.emailAddress = details[1]
+            newPerson.date = Date()
+            
+            self.prospects.add(newPerson)
+            
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = .default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+            
+            // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings() { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .sound, .badge]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("Access denied")
+                    }
+                }
+            }
         }
     }
 }
