d63dd9a339f4cd4138891d43e93293b5ab43dc50
AginS <agins.main@gmail.com>
2020-05-10 15:11:17 +0300

    day 89 is fin

--- Days 86 - 91. Project 17 (Flashzilla)/Flashzilla/Flashzilla/CardView.swift
+++ Days 86 - 91. Project 17 (Flashzilla)/Flashzilla/Flashzilla/CardView.swift
@@ -12,12 +12,27 @@ struct CardView: View {
     let card: Card
     
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    var removal: (()-> Void)? = nil
+    
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     
     var body: some View {
             ZStack {
                 RoundedRectangle(cornerRadius: 25, style: .continuous)
-                    .fill(Color.white)
+                    .fill(
+                        differentiateWithoutColor
+                            ? Color.white
+                            : Color.white
+                                .opacity(1 - Double(abs(offset.width / 50)))
+                    )
                     .shadow(radius: 10)
+                    .background(
+                        differentiateWithoutColor
+                            ? nil
+                            :  RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .fill(self.offset.width > 0 ? Color.green : Color.red)
+                    )
                 
                 VStack {
                     Text(card.prompt)
@@ -32,6 +47,25 @@ struct CardView: View {
                 .padding(20)
                 .multilineTextAlignment(.center)
             }
+            .rotationEffect(.degrees(Double(offset.width / 5)))
+            .offset(x: offset.width * 5, y: 0)
+            .opacity(2 - (Double(abs(offset.width) / 50)))
+            .gesture(
+                DragGesture()
+                    .onChanged() { gesture in
+                        self.offset = gesture.translation
+                    }
+                    .onEnded() { _ in
+                        if abs(self.offset.width) > 100 {
+                            self.removal?()
+                        } else {
+                            withAnimation {
+                                self.offset = .zero
+                            }
+                        }
+                    }
+            )
+                
             .frame(width: 450, height: 250)
             .onTapGesture {
                 withOptionalAnimation {
