07c66a12044ad364bca7fcf50b33c3d31bc257b1
AginS <agins.main@gmail.com>
2020-05-12 14:42:11 +0300

    day 94.2

--- Days 29 - 31 Project 5 (WordScramble)/WordScramble/WordScramble/ContentView.swift
+++ Days 29 - 31 Project 5 (WordScramble)/WordScramble/WordScramble/ContentView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var usedWords = [String]()
+    @State private var usedWords =  ["word1", "word2","word3", "word4", "word5", "word6", "word7", "word8", "word9","word10", "word11", "word12", "word", "word6", "word7", "word1", "word2","word3", "word4", "word5", "word6", "word7", "word8", "word9","word10", "word11", "word12", "word", "word6", "word7"] //[String]()
     @State private var rootWord = String()
     @State private var newWord = String()
     @State private var score = 0
@@ -52,6 +52,7 @@ struct ContentView: View {
     
     func startGame()
     {
+        self.usedWords = ["word1", "word2","word3", "word4", "word5", "word6", "word7", "word8", "word9","word10", "word11", "word12", "word", "word6", "word7", "word1", "word2","word3", "word4", "word5", "word6", "word7", "word8", "word9","word10", "word11", "word12", "word", "word6", "word7"]
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: ".txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
@@ -72,6 +73,7 @@ struct ContentView: View {
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
+        #if !DEBUG
         guard isOriginal(word: answer) else {
             wordError(title: "Word used already", message: "Be more original")
             return
@@ -95,6 +97,8 @@ struct ContentView: View {
             wordError(title: "Word incorrect", message: "That start word")
             return
         }
+        #endif
+        
         updateScore(word: answer)
         usedWords.insert(answer, at: 0)
         newWord = ""
@@ -102,34 +106,60 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                    .autocapitalization(.none)
-                
-                List(usedWords, id: \.self) {
-                    Image(systemName: "\($0.count).circle")
-                    Text("\($0)")
+                VStack {
+                    TextField("Enter your word", text: self.$newWord, onCommit: self.addNewWord)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        .autocapitalization(.none)
+                    
+                    GeometryReader { fullView in
+                        List(self.usedWords, id: \.self) { word in
+                            GeometryReader { geo in
+                                HStack {
+                                    Image(systemName: "\(word.count).circle")
+                                    Text("\(word)")
+                                    Spacer()
+                                }
+                                .modifier(OffsetForWord(fullViewY: fullView.frame(in: .global).maxY, minY: geo.frame(in: .global).minY))
+                                .onTapGesture {
+                                    print("fullViewY: \(fullView.frame(in: .global).maxY)")
+                                    print(" Y: \(geo.frame(in: .global).minY)")
+                                }
+                            }
+                        }
+                    }
+                    Text("Your score: \(self.score)")
+                        .font(.headline)
+                        .padding()
                 }
-                
-                Text("Your score: \(score)")
-                    .font(.headline)
-                    .padding()
-            }
-            .navigationBarTitle(rootWord)
+           // }
+            .navigationBarTitle(self.rootWord)
             .navigationBarItems(trailing:
-                Button(action: startGame, label: {
+                Button(action: self.startGame, label: {
                     Image(systemName: "arrow.clockwise")
                         .resizable()
                     .scaledToFill()
                 })
         )
-            .onAppear(perform: startGame)
-            .alert(isPresented: $showingError) {
-                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            .onAppear(perform: self.startGame)
+            .alert(isPresented: self.$showingError) {
+                Alert(title: Text(self.errorTitle), message: Text(self.errorMessage), dismissButton: .default(Text("OK")))
             }
+        
         }
+    }
+}
+
+struct OffsetForWord: ViewModifier {
+    var fullViewY: CGFloat
+    var minY: CGFloat
+    func body(content: Content) -> some View {
+        if minY > 620 {
+            return content.offset(x: (minY / fullViewY - 0.77 ) * 1200 , y: 0)
+        }
+        //print("fullViewY: \(fullViewY)")
+        //print(" Y: \(minY)")
+        return content.offset(x: 0, y: 0)
     }
 }
 
